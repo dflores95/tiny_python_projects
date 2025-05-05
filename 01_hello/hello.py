32a75aecd0bbb188d55862785819bc3e4b989e73
dflores95 <101854851+dflores95@users.noreply.github.com>
2025-05-05 15:12:45 -0500

    added argparse to push argument through bash command for executing program

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,2 +1,8 @@
-print('Hello, World!')
-  
+#!/usr/bin/env python
+# Purpose: Say hello
+import argparse
+
+parser = argparse.ArgumentParser(description='Say hello')
+parser.add_argument('name', help = 'Name to greet')
+args = parser.parse_args()
+print('Hello, ' + args.name + '!')
